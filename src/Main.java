6555ca45ebff5829f48f5e28322ae4cc86d702f1
SpeedosDK <88831904+SpeedosDK@users.noreply.github.com>
2024-09-10 13:55:53 +0200

    Opdatering..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hvilken tid på dagen? (dag, aften, nat)");
+        String time = input.nextLine();
+        System.out.println("Høt eller lav trafikmængde? (høj eller lav)");
+        String traffic = input.nextLine();
+        System.out.println("Hvilket område er det? (bolig, erhverv, blandet");
+        String areaType = input.nextLine();
+        System.out.println("Nødstilfælde (true eller false)");
+        boolean emergency = input.nextBoolean();
+
+
+
+
     }
 }

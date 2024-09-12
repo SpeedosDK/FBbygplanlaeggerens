2a625e0f7ed07b82bd29579693fb80b754e17006
ianmllr <130236399+ianmllr@users.noreply.github.com>
2024-09-12 11:04:10 +0200

    dwqdqwd

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,51 @@ public class Main {
 
         System.out.println("Hvilken tid på dagen? (dag, aften, nat)");
         String time = input.nextLine();
-        System.out.println("Høt eller lav trafikmængde? (høj eller lav)");
+        System.out.println("Høj eller lav trafikmængde? (høj, lav)");
         String traffic = input.nextLine();
-        System.out.println("Hvilket område er det? (bolig, erhverv, blandet");
+        System.out.println("Hvilket område er det? (bolig, erhverv, blandet)");
         String areaType = input.nextLine();
         System.out.println("Nødstilfælde (true eller false)");
         boolean emergency = input.nextBoolean();
+        String string1 = "";
+        String string2 = "";
 
-        if (time.equals("dag") && traffic.equals("lav") || emergency)
+        // grøn
+        if (time.equals("dag") && (traffic.equals("lav")) || emergency) {
+            //System.out.println("Trafiklyset er grønt");
+            string1 = "Trafiklyset er grønt";
+            System.out.println(string1);
+        }
+        // gul
+        else if (time.equals("aften") && (traffic.equals("lav")) || time.equals("nat") && (traffic.equals("lav"))) {
+            //System.out.println("Trafiklyset er gult");
+            string1 = "Trafiklyset er gult";
+            System.out.println(string1);
+        }
+        // rød
+        else if (time.equals("nat") && (traffic.equals("høj")) || (areaType.equals("bolig")) && (traffic.equals("høj"))) {
+            //System.out.println("Trafiklyset er rødt");
+            string1 = "Trafiklyset er rødt";
+            System.out.println(string1);
+        }
+        // adgang tilladt
+        if (areaType.equals("blandet") && traffic.equals(("lav")) || areaType.equals("erhverv") && traffic.equals("lav")) {
+            //System.out.println("Adgang tilladt");
+            string2 = "Adgang tilladt";
+            System.out.println(string2);
+        }
+        // adgang forbudt
+        if (time.equals("nat") && (areaType.equals("bolig")) && !emergency)
         {
-            System.out.println("Trafiklyset er grønt");
+            //System.out.println("Adgang forbudt");
+            string2 = "Adgang forbudt";
+            System.out.println(string2);
+        }
+        else  {
+            string2 = "Adgang tilladt";
+            System.out.println(string2);
 
         }
-
-
-
-        if (time.equals("aften") || time.equals("nat") && traffic.equals("lav"))
-        {
-            System.out.println("Trafiklyset er gul");
-
-
-        }
-        if (time.equals("nat") && traffic.equals("høj"))
-        {
-            System.out.println("Trafiklyset er rødt");
-        }
-
-
-
 
 
 
--- src/c.java
+++ src/c.java
@@ -8,7 +8,7 @@ public class c {
         String time = input.nextLine();
         System.out.println("Høj eller lav trafikmængde? (høj, lav)");
         String traffic = input.nextLine();
-        System.out.println("Hvilket område er det? (bolig, erhverv, blandet");
+        System.out.println("Hvilket område er det? (bolig, erhverv, blandet)");
         String areaType = input.nextLine();
         System.out.println("Nødstilfælde (true eller false)");
         boolean emergency = input.nextBoolean();
@@ -18,24 +18,29 @@ public class c {
             System.out.println("Trafiklyset er grønt");
         }
         // gul
-        if (time.equals("aften") && (traffic.equals("lav")) || time.equals("nat") && (traffic.equals("lav"))) {
+        else if (time.equals("aften") && (traffic.equals("lav")) || time.equals("nat") && (traffic.equals("lav"))) {
             System.out.println("Trafiklyset er gult");
         }
         // rød
-        if (time.equals("nat") && (traffic.equals("høj")) || (areaType.equals("bolig") && traffic.equals("høj"))) {
+        else if (time.equals("nat") && (traffic.equals("høj"))  || (areaType.equals("bolig") && traffic.equals("høj"))) {
             System.out.println("Trafiklyset er rødt");
         }
         // adgang tilladt
-        if (areaType.equals("blandet") && traffic.equals(("lav")) || areaType.equals("erhverv") && traffic.equals("lav")) {
+        if (areaType.equals("blandet") && traffic.equals(("lav")) || areaType.equals("erhverv") && traffic.equals("lav"))
+        {
             System.out.println("Adgang tilladt");
         }
-        // adgang forbudt
-        if (time.equals("nat") && (areaType.equals("bolig")))
-        {
-            System.out.println("Adgang forbudt");
+        // adgang forbudt medmindre emergency
+        if (emergency) {
+            System.out.println("Adgang tilladt");
         }
-        else System.out.println("Adgang tilladt"); {
+        else
+        {
+            if (time.equals("nat") && (areaType.equals("bolig")))
+            {
+            System.out.println("Adgang forbudt");
+            }
 
+            }
         }
     }
-}

bbe0d9edcb9c54c27adec26d2fd7c04b64e9374e
chr1sw0o <chrisrlh@outlook.dk>
2024-09-15 13:49:49 +0200

    df

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,31 @@ public class Main {
     {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Hvilken tid på dagen? (dag, aften, nat)");
-        String time = input.nextLine();
-        System.out.println("Høj eller lav trafikmængde? (høj, lav)");
-        String traffic = input.nextLine();
-        System.out.println("Hvilket område er det? (bolig, erhverv, blandet)");
-        String areaType = input.nextLine();
-        System.out.println("Nødstilfælde (true eller false)");
+        System.out.println("Hvilken tid på dagen? (Skriv: 'dag', 'aften' eller 'nat')");
+        String time = input.nextLine().toLowerCase();
+        System.out.println("Høj eller lav trafikmængde? (Skriv: 'høj' eller 'lav')");
+        String traffic = input.nextLine().toLowerCase();
+        System.out.println("Hvilket område er det? (Skriv: 'bolig', 'erhverv' eller 'blandet')");
+        String areaType = input.nextLine().toLowerCase();
+        System.out.println("Nødstilfælde (Skriv 'true' eller 'false')");
         boolean emergency = input.nextBoolean();
-        String string1 = "";
-        String string2 = "";
+
 
         //
-        // Grøn
+        // Vi tjekker om det er dag og der er lav trafik, og hvis det sandt er lyset grønt
         //
         if (time.equals("dag") && (traffic.equals("lav")) || emergency) {
-            string1 = "Trafiklyset er grønt";
-            System.out.println(string1);
+
+            System.out.println("Trafiklyset er grønt");
         }
         //
-        // Gul
+        // Vi tjekker om det er aften eller at det er nat og der er lav trafik på samme tid, og hvis
+        // det er sandt, er lyset gult
         //
         else if (time.equals("aften") || time.equals("nat") && (traffic.equals("lav"))) {
-            string1 = "Trafiklyset er gult";
-            System.out.println(string1);
+            System.out.println("Trafiklyset er gult");
         }
-        //
         // Rød
-        //
         /*
         else if (time.equals("nat") && (traffic.equals("høj")) || (areaType.equals("bolig") || areaType.equals("blandet")) || areaType.equals("erhverv") && (traffic.equals("høj"))) {
             //System.out.println("Trafiklyset er rødt");
@@ -42,26 +39,26 @@ public class Main {
          */
 
         // Vi indser at der er rødt lys med høj trafik lige meget situationen
+        // så vi tjekker om trafikken er høj.
 
         else if (traffic.equals("høj"))
         {
-            string1 = "Trafiklyset er rødt";
-            System.out.println(string1);
+            System.out.println("Trafiklyset er rødt");
         }
         //
-        // Adgang forbudt
+        // Adgang forbudt bliver tjekket ved at hvis tilstanden er nat og området er bolig
+        // og det ikke er en emergency
         //
         if (time.equals("nat") && (areaType.equals("bolig")) && !emergency)
         {
-            string2 = "Adgang forbudt";
-            System.out.println(string2);
+            System.out.println("Adgang forbudt");
         }
         //
-        // Adgang tilladt
+        // finder vi ud af om der er adgang tilladt, da den tjekker om der er forbudt først,
+        // ved vi der er adgang tilladt hvis der ikke er forbudt
         //
         else {
-            string2 = "Adgang tilladt";
-            System.out.println(string2);
+            System.out.println("Adgang tilladt");
         }
 
     }
